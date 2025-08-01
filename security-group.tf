5a351f9c913211f9e37e53f990acb239323dc3e8
iamcodingbaba <sahil.chhikara0727@gmail.com>
2025-08-01 16:35:51 +0530

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,23 +1,9 @@
-module "open-22-443" {
-  source = "terraform-aws-modules/security-group/aws"
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
-  name        = "user-service"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks      = ["0.0.0.0/0"]
-  ingress_rules            = ["allow 22-443"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 22
-      to_port     = 443
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 }
