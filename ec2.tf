24f42d95a19a03e478477456a0854493a853d73e
iamcodingbaba <sahil.chhikara0727@gmail.com>
2025-08-01 16:38:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,7 +9,7 @@ module "ec2_instance" {
   monitoring    = false
   subnet_id = module.vpc.public_subnets[0] # id of subnet in which you want to create ec2, the vpc and ec2 .tf files should be in the same directory to access each others output
   associate_public_ip_address = true # to get public ip in ec2
-  vpc_security_group_ids = ["sg-0c5f6e51f657a90ef"] # a security group written in aws account with desired ingress and egress rules , has to be in list
+  vpc_security_group_ids = [module.web_server_sg.security_group_id] # output taken from module web_server_sg {we can change its name in security group.tf file and then here also.}
 
 
   tags = {
