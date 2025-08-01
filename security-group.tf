e6888604c0d8d28b73e0947d21cdef5f455b2ce6
iamcodingbaba <sahil.chhikara0727@gmail.com>
2025-08-01 17:25:41 +0530

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,9 +1,20 @@
-module "web_server_sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+module "range_22_to_443_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
 
-  name        = "web-server"
-  description = "Security group for web-server with HTTP ports open within VPC"
+  name        = "range_22_443_sg"
+  description = "Security group allowing TCP traffic from port 22 to 443 from anywhere"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+      description = "Allow TCP ports 22_443 from anywhere"
+    }
+  ]
+
+  egress_rules = ["all-all"] # Allow all outbound traffic
 }
